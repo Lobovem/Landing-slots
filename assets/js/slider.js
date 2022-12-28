48a191bd1932ceaa3f6a70b10eb9f18b077049bb
Lobov Evgeniy <lobov.em@gmail.com>
2022-12-28 13:54:51 +0100

    Исправил размеры. Буду чистить код

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -23,23 +23,24 @@ const swiper = new Swiper('.swiper', {
 
   breakpoints: {
     310: {
-      slidesPerView: 2,
+      slidesPerView: 1.5,
       spaceBetween: 10,
     },
-    400: {
-      slidesPerView: 3,
+
+    550: {
+      slidesPerView: 2.7,
       spaceBetween: 20,
     },
 
     768: {
-      slidesPerView: 4,
-    },
-
-    1000: {
-      slidesPerView: 5,
+      slidesPerView: 3,
     },
 
     1300: {
+      slidesPerView: 4,
+    },
+
+    1500: {
       slidesPerView: 6,
     },
   },
